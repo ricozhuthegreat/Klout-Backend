17dc763cc0547b08297c3bead8b3a430d805c567
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-06-15 19:55:07 -0400

    Initialized Express.js

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,12 @@
 const functions = require('firebase-functions');
 
+// Express used to handle http api requests
+const express = require('express');
+const cors = require('cors');
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -8,5 +15,7 @@ const functions = require('firebase-functions');
 // });
 
 exports.influence = functions.https.onRequest((req, res) => {
+
+
 
 });
