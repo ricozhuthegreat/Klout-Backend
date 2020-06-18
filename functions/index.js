32e093a6593565ce0f608c94c50fb76f0a547ff4
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-06-18 16:10:18 -0400

    Started Firestore Listener

--- functions/index.js
+++ functions/index.js
@@ -64,4 +64,13 @@ exports.influence = functions.https.onRequest((req, res) => {
 // Cloud function used to listen for updates/publishes in businesses and assign them to influencers
 exports.assignInfluencer = functions.firestore
   .document('businesses/{userId}')
-  .onWrite((change, context) => { /* ... */ });
+  .onWrite((change, context) => {
+    // Get an object with the current document value.
+    // If the document does not exist, it has been deleted.
+    const document = change.after.exists ? change.after.data() : null;
+
+    // Get an object with the previous document value (for update or delete)
+    const oldDocument = change.before.data();
+
+    // perform desired operations ...
+  });
